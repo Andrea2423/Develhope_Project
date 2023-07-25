13cc651b50c1a128532e436086c131eb1d284c38
Giuseppe <tasso96@gmail.com>
2023-07-25 19:30:16 +0200

    .

--- src/TestHotel.java
+++ src/TestHotel.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class TestHotel {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Hotel hotelMoresco = new Hotel();
