9500ca34ba1e2e69aa1bc84ecf0d6866ad41ed8d
Andrea Zora <138602304+Andrea2423@users.noreply.github.com>
2023-07-26 15:31:57 +0200

    User

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args)  {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+
+        User user1 = new User("Andrea", "Zora", "11-1-1999", "zora.andrea@gmail.com", 123456789, "PayPal");
+        Prenotazione prenotazione1 = new Prenotazione();
+
+        prenotazione1.setUser(user1);
+        System.out.println(prenotazione1.toString());
     }
 }
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,83 @@
+package org.example;
+
+public class User {
+    private String name;
+    private String surname;
+    private String birthDate;
+    private String email;
+    private int telephoneNumber;
+
+    private String paymentMethod;
+
+
+    public User(){};
+
+    public User(String name, String surname, String birthDate, String email, int telephoneNumber, String paymentMethod) {
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.telephoneNumber = telephoneNumber;
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(int telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getPaymentMethod(){
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String metodOfPayment) {
+        this.paymentMethod = metodOfPayment;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", email='" + email + '\'' +
+                ", telephoneNumber=" + telephoneNumber + '\'' +
+                ", metodOfPayment='" + paymentMethod + '\'' + '}';
+    }
+}
