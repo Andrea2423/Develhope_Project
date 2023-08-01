aebb27457b1a048b15f9fb99fd433c914919f497
Camu <simone.benedetti94@gmail.com>
2023-08-01 19:05:33 +0200

    implementato la classe review e aggiunto l'incapsulamento alle variabili

--- src/main/java/org/example/Review_Manager.java
+++ src/main/java/org/example/Review_Manager.java
@@ -1,12 +1,19 @@
 package org.example;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Review_Manager {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Inserisca di sotto la valutazione del suo soggiorno, grazie.");
+
+        System.out.println("Prima di inserire la sua recensione deve inserire il suo nome: ");
+        String reviewerName = scanner.nextLine();
+
+        System.out.println("Data del suo soggiorno: ");
+        String stayData = scanner.nextLine();
 
         System.out.println("valutazione da 1 a 10 del suo soggiorno:");
         int hotelRating = scanner.nextInt();
@@ -15,16 +22,12 @@ public class Review_Manager {
         System.out.println("perché ?:");
         String commentHotel = scanner.nextLine();
 
-        System.out.println("data:");
-        String dataReview =scanner.nextLine();
+        LocalDateTime dataReview = LocalDateTime.now();
 
-
-        Review firstReview = new Review(hotelRating, commentHotel, dataReview);
-
-        System.out.println("valutazione data dal cliente: " + firstReview.hotelRating + "\n" +
-                "commento del cliente: " + firstReview.commentHotel + "\n" +
-                "data della recensione: " + firstReview.dataReview);
+        Review firstReview = new Review(reviewerName, stayData, hotelRating, commentHotel, dataReview);
 
         scanner.close();
+
+        firstReview.printDetails();
     }
 }
