e4f2bd1f6f9fb78460fadc97cd6f0544533d4c17
Claudio Caporaso <claudiocaporaso1998@gmail.com>
2023-08-03 15:22:54 +0200

    Ho usato l'encapsulation

--- Main.java
+++ Main.java
@@ -6,14 +6,14 @@ public class Main {
         Room room4 = new Room(4, "Suite", true, true, 5000);
         Room room5 = new Room(5, "Luxury", false, true, 10_000);
 
-        Room[] rooms = {room, room2, room3, room4, room5};
+        Room[] rooms = {room, room2, room3, room4, room5,};
         for (int i = 0; i < rooms.length ; i++) {
 
-            System.out.println("Room #" + rooms[i].roomNumber);
-            System.out.println("Room Type: " + rooms[i].roomType);
-            System.out.println("Is Occupied: " + rooms[i].isOccupied);
-            System.out.println("Is Clean: " + rooms[i].isClean);
-            System.out.println("Price: " + rooms[i].price);
+            System.out.println("Room #" + rooms[i].getRoomNumber());
+            System.out.println("Room Type: " + rooms[i].getRoomType());
+            System.out.println("Is Occupied: " + rooms[i].getIsOccupied());
+            System.out.println("Is Clean: " + rooms[i].getIsClean());
+            System.out.println("Price: " + rooms[i].getPrice());
             System.out.println("\n");
 
         }
--- Room.java
+++ Room.java
@@ -1,9 +1,49 @@
 public class Room {
-    int roomNumber;
-    String roomType;
-    boolean isOccupied;
-    boolean isClean;
-    double price;
+    private int roomNumber;
+    private String roomType;
+    private boolean isOccupied;
+    private boolean isClean;
+    private double price;
+
+    public boolean getIsOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public boolean getIsClean() {
+        return isClean;
+    }
+
+    public void setClean(boolean clean) {
+        isClean = clean;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getRoomType(){
+        return roomType;
+    }
+
+    public void setRoomType(String roomType){
+        this.roomType = roomType;
+    }
 
     public Room(int roomNumber, String roomType, boolean isOccupied, boolean isClean, double price) {
 
