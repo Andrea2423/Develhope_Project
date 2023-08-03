20fe58a90f30cace687f1906cf6370119120ca9e
Andrea Zora <138602304+Andrea2423@users.noreply.github.com>
2023-08-03 19:19:17 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,6 @@
 public class Main {
     public static void main(String[] args)  {
-        Room room = new Room(1, "Single", true, true, 225);
-        Room room2 = new Room(2, "Double", true, true, 550);
-        Room room3 = new Room(3, "Villa", false, true, 850);
-        Room room4 = new Room(4, "Suite", true, true, 5000);
-        Room room5 = new Room(5, "Luxury", false, true, 10_000);
+       
 
-        Room[] rooms = {room, room2, room3, room4, room5,};
-        for (int i = 0; i < rooms.length ; i++) {
-
-            System.out.println("Room #" + rooms[i].getRoomNumber());
-            System.out.println("Room Type: " + rooms[i].getRoomType());
-            System.out.println("Is Occupied: " + rooms[i].getIsOccupied());
-            System.out.println("Is Clean: " + rooms[i].getIsClean());
-            System.out.println("Price: " + rooms[i].getPrice());
-            System.out.println("\n");
-
-        }
-    }
+        
 }
