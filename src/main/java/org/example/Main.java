9ea9eb0f377fec4b0ebd0ec3f6e89560ec6dcfdd
Mike <109431533+1111334@users.noreply.github.com>
2023-08-28 14:07:56 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -38,58 +38,5 @@ public class Main {
         Prenotation prenotazioneEffettuata = new Prenotation(data, nomeCliente, numberPerson, prezzoInserito, reservationConfirmed, currentTime, durata);
 
         prenotazioneEffettuata.printDetails();
-
-
-        /*System.out.println(Prenotation.getData());
-        prenotazione1.setData("2023-07-25");
-        prenotazione1.setData("2023-07-25");
-
-        System.out.println(Prenotation.getNomeCliente());
-        prenotazione1.setNomeCliente("Mario Rossi");
-        prenotazione1.setNomeCliente("Mario Rossi");
-
-        System.out.println(Prenotation.getNumeroPersone());
-        prenotazione1.setNumeroPersone(1);
-        prenotazione1.setNumeroPersone(1);
-
-        System.out.println(Prenotation.getreservationConfirmed());
-        prenotazione1.setreservationConfirmed(true);
-        prenotazione1.setreservationConfirmed(true);
-
-
-        System.out.println(Prenotation.getData());
-        prenotazione2.setData("2023-08-10");
-        prenotazione1.setData("2023-08-10");
-
-        System.out.println(Prenotation.getNomeCliente());
-        prenotazione2.setNomeCliente("Luca Bianchi");
-        prenotazione1.setNomeCliente("Luca Bianchi");
-
-        System.out.println(Prenotation.getNumeroPersone());
-        prenotazione2.setNumeroPersone(1);
-        prenotazione1.setNumeroPersone(1);
-
-        System.out.println(Prenotation.getreservationConfirmed());
-        prenotazione2.setreservationConfirmed(true);
-        prenotazione1.setreservationConfirmed(true);
-
-
-        System.out.println(Prenotation.getData());
-        prenotazione3.setData("2023-09-05");
-        prenotazione1.setData("2023-09-05");
-
-        System.out.println(Prenotation.getNomeCliente());
-        prenotazione3.setNomeCliente("Giulia Verdi");
-        prenotazione1.setNomeCliente("Giulia Verdi");
-
-        System.out.println(Prenotation.getNumeroPersone());
-        prenotazione3.setNumeroPersone(1);
-        prenotazione1.setNumeroPersone(1);
-
-        System.out.println(Prenotation.getreservationConfirmed());
-        prenotazione3.setreservationConfirmed(true);
-        prenotazione1.setreservationConfirmed(true);*/
-
-
     }
 }
