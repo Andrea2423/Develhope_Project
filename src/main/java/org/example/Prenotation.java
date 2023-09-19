29d1e641355b339a5fde3fe94354b877fd7d35e5
Mike <109431533+1111334@users.noreply.github.com>
2023-09-19 12:07:38 +0200

    updated

--- src/main/java/org/example/Prenotation.java
+++ src/main/java/org/example/Prenotation.java
@@ -3,14 +3,24 @@ package org.example;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public record Prenotation(LocalDate data, String nomeCliente, int numeroPersone, LocalTime time, double price, int durataPernottamento, boolean annullata,  boolean reservationConfirmed ) {
+public record Prenotation(LocalDate data, String nomeCliente, int numeroPersone, LocalTime time, double price, int durataPernottamento, boolean calcolaTotale, String Comment, boolean annullata,  boolean reservationConfirmed ) {
 static Prenotation prenotazione;
     public String printDetails() {
         String result = "Dati personali cliente: " + nomeCliente + "\n" + "numero persone prenotate : " +
                 numeroPersone + "\n" + "data di prenotazione: " + data + "\n" + "ora di prenotazione: " + time + "\n" +
-                "durata pernottamento giorni: " + durataPernottamento + "\n" + "scegli il prezzo" + price + "\n" + "vuoi annullare la prenotazione?" + annullata + "\n" + "la prenotazione è confermata? " + reservationConfirmed;
+                "durata pernottamento giorni: " + durataPernottamento + "\n" + "scegli il prezzo" + price + "\n" + "calcola totale" + calcolaTotale + "\n" + "Commento: " + Comment
+                + "\n" + "vuoi annullare la prenotazione?" + annullata + "\n" + "la prenotazione è confermata? " + reservationConfirmed;
         return result;
     }
+
+    public double calculateTotal() {
+        return price * durataPernottamento;
+    }
+
+    public void addComment(String comment){
+        System.out.println("commento aggiunto: " + comment);
+    }
+
 };
 
 
--- src/test/java/org/example/PrenotationTest.java
+++ src/test/java/org/example/PrenotationTest.java
@@ -14,17 +14,39 @@ public class PrenotationTest {
     public void testPrintDetails() {
         Prenotation prenotation = new Prenotation(LocalDate.of(2023, 9, 7),
                 "Luca Rossi", 2, LocalTime.of(10, 0),
-                0.0, 3, false,  true);
+                0.0, 3, false, "richiesta di letto extra", false, true);
 
         String result = prenotation.printDetails();
+        System.out.println(result);
 
         assertEquals("Dati personali cliente: Luca Rossi\n" +
                 "numero persone prenotate : 2\n" +
                 "data di prenotazione: 2023-09-07\n" +
                 "ora di prenotazione: 10:00\n" +
                 "durata pernottamento giorni: 3\n" +
-                "scegli il prezzo0.0\n" + "vuoi annullare la prenotazione?false" + "\n" +
+                "scegli il prezzo0.0\n" + "calcola totalefalse\n" + "Commento: richiesta di letto extra" + "\n" + "vuoi annullare la prenotazione?false" + "\n" +
                 "la prenotazione è confermata? true", prenotation.printDetails());
     }
-}
 
+        @Test
+    public void testCalculateTotal() {
+        Prenotation prenotation = new Prenotation(LocalDate.of(2023, 9, 7),
+                "Luca Rossi", 2, LocalTime.of(10, 0),
+                100.0, 3, false, "Commento: richiesta di letto extra", false, true);
+
+        double total = prenotation.calculateTotal();
+
+        assertEquals(300.0, total, 0.01); // Assicura che il totale sia 300.0
+    }
+
+    @Test
+    public void testAddComment() {
+        Prenotation prenotation = new Prenotation(LocalDate.of(2023, 9, 7),
+                "Luca Rossi", 2, LocalTime.of(10, 0),
+                100.0, 3, false, "Commento: richiesta di letto extra", false, true);
+
+        prenotation.addComment("Commento:");
+
+        assertEquals("Commento: ", prenotation.Comment());
+    }
+}
