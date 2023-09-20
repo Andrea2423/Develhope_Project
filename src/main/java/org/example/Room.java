fc0bac3c43db972dad0650bc01f91933c840d1ef
Claudio Caporaso <claudiocaporaso1998@gmail.com>
2023-09-20 12:12:05 +0200

    Eliminata Casse RoomManager/RoomManagerTest e aggiunta la Classe RoomTest

--- src/main/java/org/example/Room.java
+++ src/main/java/org/example/Room.java
@@ -1,10 +1,15 @@
 package org.example;
 
-public class Room extends RoomManager {
+import java.util.ArrayList;
+
+public class Room {
     private int roomNumber; //numero stanza
     private String roomType; //tipologia di stanza
     private int guests;  //numero ospiti
     private double cost;  //costo stanza
+    boolean available;  //disponibile
+    boolean clean;   //pulita
+
 
     public int getGuests() {
         return guests;
@@ -54,14 +59,81 @@ public class Room extends RoomManager {
         this.roomType = roomType;
     }
 
-    public Room() {}
-
-    public Room(int guests, boolean available, boolean isClean, double cost) {
+    public Room(int roomNumber, String roomType, int guests, boolean available, boolean clean, double cost) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
         this.guests = guests;
         this.available = available;
-        this.clean = isClean;
+        this.clean = clean;
         this.cost = cost;
     }
 
+    ArrayList<String> rooms=new ArrayList<>();
 
+    public Room() {
+        rooms = new ArrayList<>();
+    }
+
+//    public void addRoom(int roomNumber, String roomType, int guest, boolean available, boolean clean, double cost) {
+//        Room addRoom = new Room();
+//        rooms.add(addRoom);
+//    }   // aggiungi stanza *DA AGGIUSTARE*
+
+    public String searchRoom(int roomNumber) {   //cerca stanza
+        for (String room : rooms) {
+            if (roomNumber == roomNumber) {
+                return room ;
+            }
+        }
+        return null;
+    } //cerca stanza
+
+//    public Room lisOfAvailableRooms() {
+//        List<Room> availableRooms = new ArrayList<>();
+//        for (String room : rooms) {
+//            if (available == true) {
+//                availableRooms.add(room);
+//            }
+//        }
+//        return null;
+//    } //elenco stanze disponibili *DA AGGIUSTARE*
+
+    public void isAvailableOrNot(int roomNumber) {
+        if (available = true) {
+            System.out.println("La stanza " + roomNumber + "è libera.");
+        } else {
+            System.out.println("La stanza " + roomNumber + "è occupata.");
+        }
+    }// è disponibile o no?
+
+    public void isCleanOrNot(Room room) {
+        if (room.clean == true) {
+            System.out.println("La stanza è pulita.");
+        } else {
+            System.out.println("La stanza NON è pulita.");
+        }
+    } // è pulita o no?
+
+    public static void canCheckIN(Room roomNumber) {
+        if (roomNumber.available == true && roomNumber.clean == true) {
+            System.out.println("La stanza " + roomNumber.getRoomNumber() + " è pronta per il checkin.");
+        } else {
+            System.out.println("La stanza " + roomNumber.getRoomNumber() + " NON è pronta per il checkin.");
+        }
+    }// disponibile per il CheckIN
+
+
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Room{");
+        sb.append("roomNumber=").append(roomNumber);
+        sb.append(", roomType='").append(roomType).append('\'');
+        sb.append(", guests=").append(guests);
+        sb.append(", cost=").append(cost);
+        sb.append(", available=").append(available);
+        sb.append(", clean=").append(clean);
+        sb.append('}');
+        return sb.toString();
+    }
 }
