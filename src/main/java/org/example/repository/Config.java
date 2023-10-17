777745df9db27c91ca8ce4071c0552c60dc7ceb4
Claudio Caporaso <claudiocaporaso1998@gmail.com>
2023-10-17 11:50:23 +0200

    I made 'public' the variables in the 'Config' class.

--- src/main/java/org/example/repository/Config.java
+++ src/main/java/org/example/repository/Config.java
@@ -2,13 +2,13 @@ package org.example.repository;
 
 public class Config {
 
-    static String NameDBdruwan = "team_project";
-    static String UsernameDBdruwan = "root";
-    static String PasswordDBdruwan = "Asdasd123stella.";
-    static String NameDBcamu = "team_project";
-    static String UsernameDBcamu = "root";
-    static String PasswordDBcamu = "developerCamu*@";
-    static String NameDBmike = "team_project";
-    static String UsernameDBmike = "root";
-    static String PasswordDBmike = "Maicoldevelhope12.";
+    public static String NameDBdruwan = "team_project";
+    public static String UsernameDBdruwan = "root";
+    public static String PasswordDBdruwan = "Asdasd123stella.";
+    public static String NameDBcamu = "team_project";
+    public static String UsernameDBcamu = "root";
+    public static String PasswordDBcamu = "developerCamu*@";
+    public static String NameDBmike = "team_project";
+    public static String UsernameDBmike = "root";
+    public static String PasswordDBmike = "Maicoldevelhope12.";
 }
