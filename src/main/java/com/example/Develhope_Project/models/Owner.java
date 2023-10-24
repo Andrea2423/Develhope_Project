d9aefceb32ad2879c8d5eea3eafaa0453b5650cf
Claudio Caporaso <claudiocaporaso1998@gmail.com>
2023-10-24 13:17:10 +0200

    Add class Owner in models

---
+++ src/main/java/com/example/Develhope_Project/models/Owner.java
@@ -0,0 +1,80 @@
+package com.example.Develhope_Project.models;
+
+import java.util.List;
+
+public class Owner {
+    private int id;
+    private String nome;
+    private String indirizzo;
+    private String email;
+    private String numeroTelefono;
+    private List<Hotel> strutture;
+    private List<Review> recensioni; //Creare un metodo che restituisce la MEDIA delle recensioni
+
+    public Owner(int id, String nome, String indirizzo, String email, String numeroTelefono, List<Hotel> strutture, List<Review> recensioni) {
+        this.id = id;
+        this.nome = nome;
+        this.indirizzo = indirizzo;
+        this.email = email;
+        this.numeroTelefono = numeroTelefono;
+        this.strutture = strutture;
+        this.recensioni = recensioni;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public List<Hotel> getStrutture() {
+        return strutture;
+    }
+
+    public void setStrutture(List<Hotel> strutture) {
+        this.strutture = strutture;
+    }
+
+    public List<Review> getRecensioni() {
+        return recensioni;
+    }
+
+    public void setRecensioni(List<Review> recensioni) {
+        this.recensioni = recensioni;
+    }
+
+}
---
+++ src/main/java/com/example/Develhope_Project/service/RoomService.java
@@ -0,0 +1,2 @@
+package com.example.Develhope_Project.service;public class RoomService {
+}
